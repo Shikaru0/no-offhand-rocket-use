29537b49a0f00055a578569867b0f044ae068e4b
Shikaru0 <games1logins1tech@gmail.com>
2026-09-20 16:44:35 +0200

    Cancel use if offhand item is FireWorkRocketItem

--- src/client/java/name/modid/client/NoOffhandRocketUseClient.java
+++ src/client/java/name/modid/client/NoOffhandRocketUseClient.java
@@ -1,10 +1,16 @@
 package name.modid.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.item.FireworkRocketItem;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 
 public class NoOffhandRocketUseClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		UseBlockCallback.EVENT.register(((player, world, hand, hitResult) ->
+			hand == Hand.OFF_HAND && player.getOffHandStack().getItem() instanceof FireworkRocketItem ? ActionResult.FAIL : ActionResult.PASS));
 	}
 }
